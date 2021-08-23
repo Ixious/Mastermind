2613baecbd075a255e3c052b1faee67163cfbb55
Mason Manuel <mason.manuel@live.com>
2021-08-23 13:55:37 +0930

    Initial commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#
+# File: mastermind.py
+# Descrition: this file is a text based version of the board game, mastermind.
+# Author: Mason Manuel
+# Student ID: 110120048
+# Email ID: manmo001
+# This is my own work as defined by
+#  the University's Academic Misconduct Policy.
+#
+class WorldOfMastermind():
+
+    reg_players = []
+
+    # run():
+    # playGame():
+    # quitGame():
+    # registerPlayer():
+    # showScoreBoard():
+    #
+    pass
+
+class Game():
+    n_players = 0
+
+    # setNGuesses():
+    # setNPlayers():
+    # checkPlayerName():
+    pass
+
+class Board():
+    n_allowed_guesses = 0
+    n_attempts = 0
+    code_guess = ""
+    guess_log = "" #+ end line, + seps etc.
+
+    #giveFeedback():
+    #testGuess():
+    #giveResult():
+    #tallyScore():
+
+    pass
+
+class Code():
+    
+    pass
+
+class GamePiece():
+    pass
+
+    class Peg():
+        pass
+    class Marbel():
+        pass
+
+
+wom = WorldOfMastermind()
+wom.run()
